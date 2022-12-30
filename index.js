7fe36c3c8fc7120233ac707cef42dfbd9bc6f403
Charles Swaleh <charlswall@live.com>
2022-12-31 01:05:11 +0300

    Completed behaviours with methods lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+class Cat{
+  constructor(name, sex){
+    this.name = name
+    this.sex = sex
+  }
+  speak(){
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog{
+  constructor(name, sex){
+    this.name = name
+    this.sex = sex
+  }
+  speak(){
+    return `${this.name} says woof!`
+  } 
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name
+    this.sex = sex
+  }
+  speak(){
+    if (this.sex === "male"){
+      return `It's me! ${this.name}, the parrot!`
+    }
+    else return `${this.name} says squawk!`
+  }
+}
+
+
+// Test data
+
+const fluffy = new Cat("Fluffy", "male")
+const scooby = new Dog("Scooby", "male")
+const cotton = new Bird("Cotton", "female")
+const birdy = new Bird("Birdie", "male")
+
+console.log(fluffy.speak())
+console.log(scooby.speak())
+console.log(cotton.speak())
+console.log(birdy.speak())
